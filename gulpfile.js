fe366618e6f5bdaa7449177f9f9b3c3856b5cac3
Páprica Comunicação <iMac_dev@iMac-de-Paprica-2.local>
2015-12-16 11:47:09 -0200

    Added install script, gulpserver fallback, package.json added

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
 
-gulp.task('webserver', function() {
+gulp.task('default', function() {
 	gulp.src('.')
 		.pipe(server({
 			livereload: true,
 			directoryListing: false,
 			open: true,
 			port: 80,
+			defaultFile: 'index.html'
+	}));
+});
+
+gulp.task('webserver', function() {
+	gulp.src('.')
+		.pipe(server({
+			livereload: true,
+			directoryListing: false,
+			open: true,
+			port: 8000,
 			defaultFile: 'index.html'
 	}));
 });
---
+++ install.sh
@@ -0,0 +1,17 @@
+ROOT_UID="0"
+if [ "$UID" -ne "$ROOT_UID" ]; then
+	echo "Running non-sudo installation"
+	npm -v >/dev/null 2>&1 || { echo >&2 "I require NPM but it's not installed.";exit 1; }
+	bower -v >/dev/null 2>&1 || { echo >&2 "I require BOWER but it's not installed. Try 'sudo npm install -g bower'";exit 1;}
+	gulp -v >/dev/null 2>&1 || { echo >&2 "I require GULP but it's not installed. Try 'sudo npm install -g gulp'";exit 1;}
+else
+	npm -v >/dev/null 2>&1 || { echo >&2 "I require NPM but it's not installed.";exit 1; }
+	echo 'Checking BOWER...'
+	bower -v >/dev/null 2>&1 || { echo 'BOWER not found, installing ...'; npm install -g bower; }
+	echo 'Checking GULP...'
+	gulp -v >/dev/null 2>&1 || { echo 'GULP not found, installing ...'; npm install -g gulp; }
+fi
+
+npm install
+bower install
+gulp || gulp webserver
